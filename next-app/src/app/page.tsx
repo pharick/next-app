2ce9bdfeb6f5a1b5b6e0ca0745009583a00680a8
phar <artemforkunov@gmail.com>
2024-03-08 19:49:08 +0700

    update

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>Hello github ci</h1>;
+  return (
+    <>
+      <h1>Hello github ci</h1>
+      <p>New line</p>
+    </>
+  );
 }
